990326fed7766537119892ab119a230aef177d7d
hacker6291 <98105494+hacker6291@users.noreply.github.com>
2022-02-26 02:07:48 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,19 @@
+clear
+echo -e "$Yellow                            processing please wait..>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait...>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait....>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait.....>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait......>"
+sleep 2.0
+clear
+echo " "
+cd InstaUnfollow
+python3 unfollow.py
